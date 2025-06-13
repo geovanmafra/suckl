3e28e76c34b2a744ec1f314c905c9b24e46506f9
kuriboh <geovanmafra@proton.me>
2025-06-13 17:27:52 -0300

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -94,6 +94,9 @@
     services.pipewire = {
       enable = true;
       pulse.enable = true;
+      jack.enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
     };
 
     # Enable touchpad support (enabled default in most desktopManager).
@@ -101,6 +104,36 @@
 
     # Enable the OpenSSH daemon.
     openssh.enable = true;
+
+    # Enable autologin into TTY.
+    getty = {
+      autologinOnce = true;
+      autologinUser = "user";
+    };
+
+    # Automounting support.
+    udisks2.enable = true;
+
+    # Battery management.
+    tlp = {
+      enable = true;
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+        CPU_MIN_PERF_ON_AC = 0;
+        CPU_MAX_PERF_ON_AC = 100;
+        CPU_MIN_PERF_ON_BAT = 0;
+        CPU_MAX_PERF_ON_BAT = 20;
+
+        # Optional helps save long term battery health.
+        START_CHARGE_THRESH_BAT0 = 40; # 40 and below it starts to charge.
+        STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging.
+      };
+    };
   };
 
   # Copy the NixOS configuration file and link it from the resulting system
