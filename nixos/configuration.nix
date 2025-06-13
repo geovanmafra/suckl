d8c8b087be15254ef4e472588b7e5c9f4309acbe
kuriboh <geovanmafra@proton.me>
2025-06-13 17:29:35 -0300

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -56,8 +56,8 @@
 
   # List packages installed in system profile. You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
+    kdePackages.dolphin
   ];
 
   # Some programs need SUID wrappers, can be configured further or are started in user sessions.
@@ -68,6 +68,7 @@
     #   enableSSHSupport = true;
     # };
 
+    # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     nano.enable = false;
     neovim = {
       enable = true;
