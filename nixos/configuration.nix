dbd97dc76a309a6255532675a0a05b769630a10f
kuriboh <geovanmafra@proton.me>
2025-06-13 18:51:38 -0300

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -11,7 +11,7 @@ let
   };
 
   # Import wallpaper from GitHub.
-  wallpaper = pkgs.fetchurl {
+  wallpaper = pkgs.fetchFromGitHub {
     url = "https://raw.githubusercontent.com/geovanmafra/nixos/refs/heads/main/wallpaper.jpg";
   };
 in
